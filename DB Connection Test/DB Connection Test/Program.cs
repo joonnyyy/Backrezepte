bd3339fdab88fe77a6ffe550abc9e876f98bbb91
Niklas <Admin@LAPTOP>
2017-04-05 10:23:55 +0200

    .

--- DB Connection Test/DB Connection Test/Program.cs
+++ DB Connection Test/DB Connection Test/Program.cs
@@ -10,32 +10,26 @@ namespace database_demo1
             // http://www.connectionstrings.com/sql-server/
             // use "." for local server like: '.\myInstanceName'
             string connectionString = @"Server=192.168.56.101;Database=testdb;User Id=Admin;Password=12345678;";
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    string commandstring = "SELECT * FROM test";
-                    SqlCommand command = new SqlCommand(commandstring, connection);
-                    SqlDataReader reader;
-                    connection.Open();
-                    DataTable columns = connection.GetSchema("Columns");
-                    foreach (DataRow row in columns.Rows)
-                    {
-                        Console.WriteLine(row["COLUMN_NAME"].ToString());
-                        Console.WriteLine(row["TABLE_NAME"].ToString());
-                    }
-                    reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine(reader["nr"] + "\t" + reader["name"] );
-                    }
-                    Console.ReadKey();
+            SqlConnection connection; SqlCommand command;
+            string sql = null; SqlDataReader dataReader;
+   
+            sql = "Select * From test";
+            connection = new SqlConnection(connectionString);
+            try {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read()) {
+                    Console.WriteLine(dataReader.GetValue(0) + " - " + dataReader.GetValue(1));
                 }
-            }catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
             }
-            Console.ReadKey();
+            catch (Exception ex) {
+                Console.WriteLine("Can not open connection !: " + ex.Message);
+            }
+            Console.Read();
         }
     }
 }
