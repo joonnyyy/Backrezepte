e935948cf558e99f95e4c6b2216c05de25c24493
Rosty <Cassan@LAPTOP-KFJNSGVI>
2017-04-05 11:26:18 +0200

    v111111

--- DB TEST 2/DB TEST 2/Form1.Designer.cs
+++ DB TEST 2/DB TEST 2/Form1.Designer.cs
@@ -59,7 +59,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(720, 514);
+            this.button2.Location = new System.Drawing.Point(95, 522);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
